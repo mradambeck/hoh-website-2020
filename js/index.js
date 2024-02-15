37a32dff21389e41483b49ef76624b758ec3c2c2
Adam Beck <adambeck@Adams-MacBook-Pro.local>
2024-02-15 11:25:03 -0800

    Adjust video player functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-function showVideo () {
-  const logo = document.querySelector('.logo');
-  const video = document.querySelector('.video');
-  logo.classList.add('hidden');
-  video.classList.remove('hidden');
-};
+function showVideo() {
+  const logo = document.querySelector(".logo");
+  const video = document.querySelector(".video");
+
+  video.innerHTML = `
+<iframe
+  class="youtube-iframe"
+  src="https://www.youtube.com/embed/uw6M5uzlX2A"
+  title="YouTube video player"
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen
+></iframe>
+  `;
+
+  logo.classList.add("hidden");
+  video.classList.remove("hidden");
+}
